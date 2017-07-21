b03cf82f7ca616fc51d2a08c380da40ae338b53d
Mr-Yellow <mr-yellow@mr-yellow.com>
2017-07-21 12:46:11 +1000

    Add unittest

---
+++ gym_mazeexplorer/test/test_mazeexplorerenv.py
@@ -0,0 +1,30 @@
+import unittest
+
+import gym
+from gym_mazeexplorer.envs.mazeexplorer_env import MazeExplorerEnv
+from time import sleep
+
+class TestMazeExplorerEnv(unittest.TestCase):
+
+    def test_env(self):
+
+        env = MazeExplorerEnv()
+        env.reset()
+        for _ in range(50):
+
+            #env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+
+            #self.assertTrue(type(observation) == numpy.ndarray)
+            self.assertTrue(type(reward) == int or type(reward) == float)
+            self.assertTrue(type(done) == bool)
+            self.assertTrue(type(info) == dict)
+
+            sleep(0.05)
+
+        return True
+
+
+if __name__ == '__main__':
+    unittest.main()
