f5ac7ec5f27770f32d281256b0b1c66c2f16912c
Mr-Yellow <mr-yellow@mr-yellow.com>
2017-07-25 17:58:47 +1000

    Package classifiers

--- setup.py
+++ setup.py
@@ -2,14 +2,34 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='gym_mazeexplorer',
-      version='0.0.1',
-      author='Mr-Yellow',
-      author_email='mr-yellow@mr-yellow.com',
-      description='A maze exploration environment for openai/gym',
-      packages=find_packages(),
-      url='https://github.com/mryellow/gym-mazeexplorer',
-      license='MIT',
-      install_requires=['gym', 'mazeexp']
+    version='0.0.1',
+    author='Mr-Yellow',
+    author_email='mr-yellow@mr-yellow.com',
+    description='A maze exploration environment for openai/gym',
+    packages=find_packages(),
+    url='https://github.com/mryellow/gym-mazeexplorer',
+    license='MIT',
+    install_requires=['gym', 'mazeexp'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
 
 
